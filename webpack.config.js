2223ed8cbeb43c8992b9e82bd59459bf4212aa4f
QuangBui <qaker1035@gmail.com>
2022-10-15 11:11:55 +0700

    Update code structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const path = require("path");
 module.exports = {
     entry: {
         "/dist/bundle": "./src/index.js",
-        "/firebase-messaging-sw": "./src/firebase-messaging-sw.js",
+        "/dist/firebase-messaging-sw": "./src/firebase-messaging-sw.js",
     },
     output: {
         path: __dirname,
         filename: "[name].js",
     },
     watch: true,
+    mode: "development",
 };
