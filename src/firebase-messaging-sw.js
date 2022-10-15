475c4bb87f62c31816122c5c6e59a35debb93d51
QuangBui <qaker1035@gmail.com>
2022-10-15 10:56:59 +0700

    add: firebase message cloud

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,33 +1,22 @@
 import { onBackgroundMessage } from "firebase/messaging/sw";
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging/sw";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAtDKP9YiaDYqv2nWhgLHcXATstLiiVa_I",
-    authDomain: "fir-tutorial-5fd63.firebaseapp.com",
-    projectId: "fir-tutorial-5fd63",
-    storageBucket: "fir-tutorial-5fd63.appspot.com",
-    messagingSenderId: "862342591646",
-    appId: "1:862342591646:web:85af73192598cc6396778f",
-};
+import { firebaseConfig } from "../firebase.config";
 
 initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = getMessaging();
 
 onBackgroundMessage(messaging, (payload) => {
-    console.log(
-        "[firebase-messaging-sw.js] Received background message ",
-        payload
-    );
-    // Customize notification here
-    // const notificationTitle = "Background Message Title";
-    // const notificationOptions = {
-    //     body: "Background Message body.",
-    //     icon: "/firebase-logo.png",
-    // };
+    console.log("Received background message ", payload);
 
-    // self.registration.showNotification(notificationTitle, notificationOptions);
+    // console.log({ ...payload.data });
+    // Customize notification here
+    const notificationTitle = payload.data.title;
+    const notificationOptions = {
+        body: payload.data.message,
+        icon: "/firebase-logo.png",
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-require("dotenv").config();
+import { firebaseConfig, vapidKey } from "../firebase.config";
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-};
+initializeApp(firebaseConfig);
 
 //init firebase app
 const app = initializeApp(firebaseConfig);
@@ -36,9 +29,7 @@ getDocs(collectionRef)
 // Get registration token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
 const messaging = getMessaging(app);
-getToken(messaging, {
-    vapidKey: process.env.VAPID_KEY,
-})
+getToken(messaging, { vapidKey })
     .then((currentToken) => {
         if (currentToken) {
             // Send the token to your server and update the UI if necessary
